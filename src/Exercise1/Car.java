515ba7181348ff1c1f54ff06bc3d5e52086b421c
Aliana Tejeda <aliana124@gmail.com>
2021-04-29 00:56:16 -0400

    Finishing up

---
+++ src/Exercise1/Car.java
@@ -0,0 +1,29 @@
+package Exercise1;
+
+public class Car
+{
+    private int fuel;
+
+    public Car() { fuel = 0; }
+    public Car(int g) { fuel = g; }
+
+    public void addFuel() { fuel++; }
+    public void display() { System.out.print(fuel + " "); }
+}
+
+public class RaceCar extends Car
+{
+    public RaceCar(int g) { super(2*g); }
+
+    public static void main(String[] args)
+    {
+        Car car = new Car(5);
+        Car fastCar = new RaceCar(5);
+        car.display();
+        car.addFuel();
+        car.display();
+        fastCar.display();
+        fastCar.addFuel();
+        fastCar.display();
+    }
+}
